cd91218f847aaf54add9bc3df7f247cd7a84e325
VitaPad <v.mars22@ukr.net>
2024-01-31 18:33:22 +0200

    add index.html

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+const handleClick = (event) => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+};
+button.addEventListener("click", handleClick);
