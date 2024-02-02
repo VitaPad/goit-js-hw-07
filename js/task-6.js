303422e0659132f7dd7ac23e4d78e28f50421f99
VitaPad <v.mars22@ukr.net>
2024-02-02 11:20:49 +0200

    add index.html

--- js/task-6.js
+++ js/task-6.js
@@ -3,7 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const containerEl = document.querySelector("№controls");
+const boxEl = document.querySelector("#boxes");
 const inputEl = document.querySelector(".number");
-const buttonEl = document.querySelector(".button");
-hiddenButton.addEvenListener("click", () => {});
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+let size = 30;
+buttonCreate.addEventListener("click", () => {
+  const newBox = document.createElement("div");
+  size += 10;
+  newBox.style.width = `${size}px`;
+  newBox.style.height = `${size}px`;
+  newBox.style.backgroundColor = getRandomHexColor();
+
+  boxEl.appendChild(newBox);
+});
+
+/* destroyBoxes.addEvenListener("click", () => {
+if (){}
+
+}); */
+
+/* buttonCreate.addEventListener("click", createBoxes);
+buttonDestroy.addEventListener("click", destroyBoxes); */
